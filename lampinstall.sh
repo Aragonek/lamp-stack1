107be9ec0a1ec5d71393f8dadc841e43e0067197
Aragonek <131952512+Aragonek@users.noreply.github.com>
2023-04-27 12:24:08 +0200

    Update lampinstall.sh

--- lampinstall.sh
+++ lampinstall.sh
@@ -45,16 +45,7 @@ dnf install vsftpd
 rpm -qi vsftpd
 
 systemctl start vsftpd
-systemctl enable vsftpd <<EOF
-
-  echo /etc/vsftpd/vsftpd.conf | >> pasv_min_port=30000 pasv_max_port=31000 userlist_file=/etc/vsftpd/user_list userlist_deny=NO
-EOF
-
-systemctl restart vsftpd
-
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/vsftpd.pem -out /etc/vsftpd/vsftpd.pem <<EOF
-  echo /etc/vsftpd/vsftpd.conf | >> rsa_cert_file=/etc/vsftpd/vsftpd.pem rsa_private_key_file=/etc/vsftpd.pem ssl_enable=ON
-EOF
+systemctl enable vsftpd 
 
 firewall-cmd --add-service=http --permanent
 firewall-cmd --add-service=https --permanent
